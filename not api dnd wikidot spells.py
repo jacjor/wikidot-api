acf0deaa8cbd689d24d27d7b054d160530767ba4
jacjor <155621163+jacjor@users.noreply.github.com>
2024-09-20 19:55:03 +0100

    original

--- not api dnd wikidot spells.py
+++ not api dnd wikidot spells.py
@@ -1,17 +1,3 @@
-'''root values lineage,
-spell,
-background,
-feat
-
-sub values
-no root    adventuring gear,armor,trinkets,weapons,firearms,explosives,wondrous items,currency,poisons,tools,siege equipment
-no root artificer,barbarian...
-name of feat
-name of spell
-name of background
-put false if there is no root value
-'''
-
 import webbrowser
 root = 'false'
 while True :
